0dc6878f4c850b8f30c37bf22335e376b6742e39
AlanEzeq <57920213+AlanEzeq@users.noreply.github.com>
2020-12-04 00:31:14 -0300

    adminVista

---
+++ src/components/Reserva.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Header from "../shared/Header"
+import Container from "@material-ui/core/Container"
+
+const Reserva = (prop) => {
+    return (  
+    
+        <div>
+        <Header/>
+             <div>
+          <Container maxWidth="sm">
+            <h1>reservasdsada</h1>
+            <h1>Deshasdasd</h1>
+            </Container>
+            </div>
+        </div>
+
+
+    
+
+    )
+}
+
+
+export default Reserva;
